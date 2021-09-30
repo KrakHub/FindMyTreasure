a28c0b8bf36a4f122caabeb768b23bd1a7454b66
Antonio Cotto <superajc@outlook.com>
2021-09-30 12:38:24 -0400

    Implemented a location tracker & score system.

--- game.js
+++ game.js
@@ -1,20 +1,49 @@
 let xcord = Math.random();
 let ycord = Math.random();
+let clicks = 0;
 console.log('xcord = ' + xcord)
 console.log('ycord = ' + ycord)
-
+//document.getElementById("treasure").innerHTML = "Winning coordinates: (" + Math.floor(xcord*window.innerWidth) + "," + Math.floor(ycord*window.innerHeight) + ")";
 
 window.onload = function()
 {
     window.onclick = function(e)
     {
         let evt = window.event || e;
-        document.getElementById("result").innerHTML = "Mouse position: (" + evt.clientX + "," + evt.clientY + ")"
+        document.getElementById("result").innerHTML = "Mouse position: (" + evt.clientX + "," + evt.clientY + ")";
+        clicks++;
+        document.getElementById("clicks").innerHTML = clicks;
 
         if ((evt.clientX < Math.floor(xcord*window.innerWidth) + 20 && evt.clientX > Math.floor(xcord*window.innerWidth) - 20) && (evt.clientY < Math.floor(ycord*window.innerHeight) + 20 && evt.clientY > Math.floor(ycord*window.innerHeight) - 20)) {
             alert('You Win!');
         }
-            console.log('xcordfloor = ' + Math.floor(xcord*window.innerWidth))
-            console.log('ycordfloor = ' + Math.floor(ycord*window.innerHeight))
+        else if ((evt.clientX < Math.floor(xcord*window.innerWidth) + 35 && evt.clientX > Math.floor(xcord*window.innerWidth) - 35) && (evt.clientY < Math.floor(ycord*window.innerHeight) + 35 && evt.clientY > Math.floor(ycord*window.innerHeight) - 35)) {
+            document.getElementById("prox").innerHTML = "🔥🔥🔥";
+        }
+        else if ((evt.clientX < Math.floor(xcord*window.innerWidth) + 50 && evt.clientX > Math.floor(xcord*window.innerWidth) - 50) && (evt.clientY < Math.floor(ycord*window.innerHeight) + 50 && evt.clientY > Math.floor(ycord*window.innerHeight) - 50)) {
+            document.getElementById("prox").innerHTML = "🔥🔥";
+        }
+        else if ((evt.clientX < Math.floor(xcord*window.innerWidth) + 75 && evt.clientX > Math.floor(xcord*window.innerWidth) - 75) && (evt.clientY < Math.floor(ycord*window.innerHeight) + 75 && evt.clientY > Math.floor(ycord*window.innerHeight) - 75)) {
+            document.getElementById("prox").innerHTML = "🔥";
+        }
+        else if ((evt.clientX < Math.floor(xcord*window.innerWidth) + 100 && evt.clientX > Math.floor(xcord*window.innerWidth) - 100) && (evt.clientY < Math.floor(ycord*window.innerHeight) + 100 && evt.clientY > Math.floor(ycord*window.innerHeight) - 100)) {
+            document.getElementById("prox").innerHTML = "🌶️";
+        }
+        else if ((evt.clientX < Math.floor(xcord*window.innerWidth) + 250 && evt.clientX > Math.floor(xcord*window.innerWidth) - 250) && (evt.clientY < Math.floor(ycord*window.innerHeight) + 250 && evt.clientY > Math.floor(ycord*window.innerHeight) - 250)) {
+            document.getElementById("prox").innerHTML = "💦";
+        }
+        else if ((evt.clientX < Math.floor(xcord*window.innerWidth) + 500 && evt.clientX > Math.floor(xcord*window.innerWidth) - 500) && (evt.clientY < Math.floor(ycord*window.innerHeight) + 500 && evt.clientY > Math.floor(ycord*window.innerHeight) - 500)) {
+            document.getElementById("prox").innerHTML = "❄️";
+        }
+        else {
+            document.getElementById("prox").innerHTML = "🧊";
+        } 
     }
+    console.log('xcordfloor = ' + Math.floor(xcord*window.innerWidth))
+    console.log('ycordfloor = ' + Math.floor(ycord*window.innerHeight))
+    window.onresize = function()
+    {
+       document.getElementById("treasure").innerHTML = "Winning coordinates: (" + Math.floor(xcord*window.innerWidth) + "," + Math.floor(ycord*window.innerHeight) + ")";
+    }
+    
 }
